0726bb8941e76db59b8ecea6290500315413ad9e
ryann al-lahiq <ryann.allahiq@gmail.com>
2020-09-01 18:31:13 +0100

    updated

--- src/pages/testpage.js
+++ src/pages/testpage.js
@@ -7,6 +7,7 @@ export default () => {
             <h1>this is a test page to test the autorouting form gatsby</h1>
             <h2>this is a test page to test the autorouting form gatsby</h2>
             <h3>this is a test page to test the autorouting form gatsby</h3>
+            <h5>this is a test page to test the autorouting form gatsby</h5>
         </div>
     )
 
