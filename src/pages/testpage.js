9af73c44bea7746084f6414a72b861a2b665c4c4
ryann al-lahiq <ryann.allahiq@gmail.com>
2020-09-01 18:30:36 +0100

    changed page heading structure

--- src/pages/testpage.js
+++ src/pages/testpage.js
@@ -4,10 +4,9 @@ export default () => {
     return(
         <div>
 
-            this is a test page to test the autorouting form gatsby
-            this is a test page to test the autorouting form gatsby
-            this is a test page to test the autorouting form gatsby
-            this is a test page to test the autorouting form gatsby
+            <h1>this is a test page to test the autorouting form gatsby</h1>
+            <h2>this is a test page to test the autorouting form gatsby</h2>
+            <h3>this is a test page to test the autorouting form gatsby</h3>
         </div>
     )
 
